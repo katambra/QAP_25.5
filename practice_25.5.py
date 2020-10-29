a5a97125117b6f121fc91e233557c7834cad8146
katambra <66130676+katambra@users.noreply.github.com>
2020-10-29 23:20:16 +0300

    Add files via upload

---
+++ practice_25.5.py
@@ -0,0 +1,74 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture(autouse=True)
+def testing():
+    pytest.driver = webdriver.Chrome('C:/ProgramData/chromedriver.exe')
+    # Переходим на страницу авторизации
+    pytest.driver.get('http://petfriends1.herokuapp.com/login')
+
+    yield
+
+    pytest.driver.quit()
+
+
+def test_show_my_pets():
+    # Вводим email
+    pytest.driver.find_element_by_id('email').send_keys('vasya@mail.com')
+    # Вводим пароль
+    pytest.driver.find_element_by_id('pass').send_keys('12345')
+    # Нажимаем на кнопку входа в аккаунт
+    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+    pytest.driver.implicitly_wait(10)
+    element = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    images = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    names = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    descriptions = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+
+    for i in range(len(names)):
+        assert images[i].get_attribute('src') != ''
+        assert names[i].text != ''
+        assert descriptions[i].text != ''
+        assert ', ' in descriptions[i]
+        parts = descriptions[i].text.split(", ")
+        assert len(parts[0]) > 0
+        assert len(parts[1]) > 0
+
+
+def test_pets_table():
+    # Вводим email
+    pytest.driver.find_element_by_id('email').send_keys('vasya@mail.com')
+    # Вводим пароль
+    pytest.driver.find_element_by_id('pass').send_keys('12345')
+    # Нажимаем на кнопку входа в аккаунт
+    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+    element = WebDriverWait(pytest.driver, 10).until(
+        EC.presence_of_element_located(By.CSS_SELECTOR('.card-deck .card-img-top'))
+    )
+
+    lines = pytest.driver.find_elements_by_xpath('//*[@id="all_my_pets"]/table/tbody/tr')
+    images = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    names = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    descriptions = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+
+    for i in range(len(lines)):
+        assert len(lines) == 977
+        assert images >= len(lines) / 2
+        assert names[i].text != ''
+        assert descriptions[i].text != ''
+        assert ', ' in descriptions[i]
+        parts = descriptions[i].text.split(", ")
+        assert len(parts[0]) > 0
+        assert len(parts[1]) > 0
+        a = set(names)
+        assert len(lines) == len(a)
